7c3515048dbfeb382c04f204f6365c5a972616cc
BhaveerH <2116470@students.wits.ac.za>
2022-11-14 16:35:35 +0200

    edited pressures

--- Group13GDProject/Assets/Scripts/SpikePressure.cs
+++ Group13GDProject/Assets/Scripts/SpikePressure.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class SpikePressure : MonoBehaviour
 {
-    public float timerCountUp, timerCountDown = 5, randomX;
+    public float timerCountUp, timerCountDown = 5;
     public bool isZero = false;
     public GameObject gCam, pbSpike;
+    public int randomX;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +24,17 @@ public class SpikePressure : MonoBehaviour
         if (timerCountDown <= 0)
         {
             timerCountDown = 0;
-            randomX = Random.Range(-8f, 8f);
-            Instantiate(pbSpike, new Vector2(randomX, gCam.transform.position.y + 5), transform.rotation);
+            randomX = Random.Range(1, 3);
+
+            if (randomX == 1)
+            {
+                Instantiate(pbSpike, new Vector2(-2.9f, gCam.transform.position.y + 5), transform.rotation);
+            }
+            else
+            {
+                Instantiate(pbSpike, new Vector2(3.17f, gCam.transform.position.y + 5), transform.rotation);
+            }
+            
             isZero = true;
         }
 
