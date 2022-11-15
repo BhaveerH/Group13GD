25321968cdc22aa58a1b60485a74cc3fa69e80f2
BhaveerH <2116470@students.wits.ac.za>
2022-11-15 18:27:13 +0200

    sprite changes for bullets and slight hover

--- Group13GDProject/Assets/Scripts/BulletCollect.cs
+++ Group13GDProject/Assets/Scripts/BulletCollect.cs
@@ -6,20 +6,66 @@ public class BulletCollect : MonoBehaviour
 {
 
     private PlayerControl playerControl;
-   // public Sprite spBullet;
+    // public Sprite spBullet;
     //private SpriteRenderer srBullet;
+    public float speed = 0.4f;
+    Vector2 StartPos;
+    Vector2 EndPos;
+    public float HoverDistance = 0.5f;
+
+    private float startPosx;
+    private bool moveUp;
+    private bool isStartTrue = false;
     // Start is called before the first frame update
     void Start()
     {
         playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
-      //  srBullet = this.gameObject.GetComponent<SpriteRenderer>();
-       // srBullet.sprite = srBullet.sprite;
+        StartPos = transform.position;
+        EndPos = StartPos + new Vector2(0, HoverDistance);
+        //  srBullet = this.gameObject.GetComponent<SpriteRenderer>();
+        // srBullet.sprite = srBullet.sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        startPosx = transform.position.x;
+        if (StartPos.x == startPosx)
+        {
+            isStartTrue = true;
+        }
+        else
+        {
+            isStartTrue = false;
+        }
+
+        if (isStartTrue == false)
+        {
+            EndPos = StartPos;
+            StartPos = transform.position;
+            EndPos += StartPos;
+        }
+
+        if (transform.position.y == StartPos.y)
+        {
+            moveUp = true;
+        }
+        else if (transform.position.y == EndPos.y)
+        {
+            moveUp = false;
+        }
+
+        if (moveUp == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, EndPos, 1 * speed * Time.deltaTime);
+        }
+
+        if (moveUp == false)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, StartPos, 1 * speed * Time.deltaTime);
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
