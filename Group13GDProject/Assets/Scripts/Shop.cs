b9cba9a8d6687d4703ff5ba63aaaac9a0fc9f37e
BhaveerH <2116470@students.wits.ac.za>
2022-11-15 12:44:04 +0200

    shop for levels
    developed code for the shop to access and buy the levels

--- Group13GDProject/Assets/Scripts/Shop.cs
+++ Group13GDProject/Assets/Scripts/Shop.cs
@@ -6,61 +6,52 @@ using UnityEngine.SceneManagement;
 
 public class Shop : MonoBehaviour
 {
-    public Button btnLevel1, btnLevel2, btnLevel3, btnLevel4, btnLevel5;
-    public int l1Cost, l2Cost, l3Cost, l4Cost, l5Cost;
+    public Button btnLevel2, btnLevel3, btnLevel4, btnLevel5, btnOkay;
+    public int l4Cost, l5Cost, lNum;
+    public float fTimeDelay = 0.5f;
+    public Text tCoinsTotal;
+    public GameObject g4Coin, g4Text, g5Coin, g5Text, gPanel;
     // Start is called before the first frame update
     void Start()
     {
-        btnLevel1.onClick.AddListener(level1);
         btnLevel2.onClick.AddListener(level2);
         btnLevel3.onClick.AddListener(level3);
         btnLevel4.onClick.AddListener(level4);
         btnLevel5.onClick.AddListener(level5);
+        btnOkay.onClick.AddListener(bOkay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-
-    public void level1()
-    {
-        CoinsTotal.coinTotal = CoinsTotal.coinTotal - l1Cost;
-
-        ShopVarHolder.bLevel2 = true;
-
-        // put next scene
+        tCoinsTotal.text = CoinsTotal.coinTotal.ToString();
     }
 
     public void level2()
     {
-        if ((ShopVarHolder.bLevel2 == true) && (CoinsTotal.coinTotal - l2Cost >= 0))
-        {
-            ShopVarHolder.bLevel3 = true;
-
-            // put next scene
-        }
+        lNum = 2;
+        StartCoroutine(TimeDelay());
     }
 
     public void level3()
     {
-        if ((ShopVarHolder.bLevel3 == true) && (CoinsTotal.coinTotal - l3Cost>= 0))
-        {
-            ShopVarHolder.bLevel4 = true;
-
-            // put next scene
-        }
+        lNum = 3;
+        StartCoroutine(TimeDelay());
     }
 
     public void level4()
     {
         if ((ShopVarHolder.bLevel4 == true) && (CoinsTotal.coinTotal - l4Cost >= 0))
         {
-            ShopVarHolder.bLevel5 = true;
-
-            // put next scene
+            lNum = 4;
+            g4Coin.SetActive(false);
+            g4Text.SetActive(false);
+            CoinsTotal.coinTotal -= l4Cost;
+            StartCoroutine(TimeDelay());
+        }
+        else
+        {
+            gPanel.SetActive(true);
         }
     }
 
@@ -68,10 +59,40 @@ public class Shop : MonoBehaviour
     {
         if ((ShopVarHolder.bLevel5 == true) && (CoinsTotal.coinTotal - l5Cost >= 0))
         {
-            
-
-            // put next scene
+            lNum = 5;
+            g5Coin.SetActive(false);
+            g5Text.SetActive(false);
+            CoinsTotal.coinTotal -= l5Cost;
+            StartCoroutine(TimeDelay());
         }
+        else
+        {
+            gPanel.SetActive(true);
+        }
+    }
+
+    IEnumerator TimeDelay()
+    {
+        yield return new WaitForSeconds(fTimeDelay);
+        switch (lNum)
+        {
+            case 2: SceneManager.LoadScene("WATER LEVEL (L2) 1");
+                break;
+            case 3: ShopVarHolder.bLevel4 = true;
+                    SceneManager.LoadScene("DESERT LEVEL (L3)");
+                break;
+            case 4: ShopVarHolder.bLevel5 = true;
+                    SceneManager.LoadScene("ICE LEVEL (L4)");
+                break;
+            case 5: SceneManager.LoadScene("FIRE  LEVEL (L5)");
+                break;
+        }
+    }
+
+
+    public void bOkay()
+    {
+        gPanel.SetActive(false);
     }
 
 }
--- Group13GDProject/Assets/Scripts/ShopVarHolder.cs
+++ Group13GDProject/Assets/Scripts/ShopVarHolder.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ShopVarHolder : MonoBehaviour
 {
-    public static bool bLevel1 = true, bLevel2 = false, bLevel3 = false, bLevel4 = false, bLevel5 = false;
+    public static bool bLevel2 = true, bLevel3 = true, bLevel4 = false, bLevel5 = false;
     // Start is called before the first frame update
     void Start()
     {
